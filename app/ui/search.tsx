f6427adf282411feb407e192da4dd54b4b09e7c8
hayapy88 <payagram01@gmail.com>
2025-06-22 23:56:23 +1000

    Fix a typo

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -9,7 +9,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const { replace } = useRouter();
 
   function handleSearch(term: string) {
-    const params = new URLSearchPrams(searchParams);
+    const params = new URLSearchParams(searchParams);
     if (term) {
       params.set("query", term);
     } else {

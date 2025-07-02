9261ddf7221cdf537b77d1e408112c3ab60ba164
hayapy88 <payagram01@gmail.com>
2025-07-02 23:55:33 +1000

    Add error-handling page for invoices

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -69,6 +69,7 @@ export async function updateInvoice(id: string, formData: FormData) {
 
 // DeleteInvoice function to delete an invoice by id
 export async function deleteInvoice(id: string) {
+  throw new Error("Failed to Delete Invoice");
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath("/dashboard/invoices");

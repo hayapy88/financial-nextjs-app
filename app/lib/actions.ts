26ef33e3e31c08378563b7aad4264947d94f7997
hayapy88 <payagram01@gmail.com>
2025-07-04 00:04:00 +1000

    Handle 404 error

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -69,7 +69,7 @@ export async function updateInvoice(id: string, formData: FormData) {
 
 // DeleteInvoice function to delete an invoice by id
 export async function deleteInvoice(id: string) {
-  throw new Error("Failed to Delete Invoice");
+  // throw new Error("Failed to Delete Invoice");
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath("/dashboard/invoices");

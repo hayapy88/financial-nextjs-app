4590b194e29f6147eba143798b3cea737fd427c8
hayapy88 <payagram01@gmail.com>
2025-07-07 21:13:13 +1000

    Add the other fields' error messages

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -62,7 +62,9 @@ export async function createInvoice(prevState: State, formData: FormData) {
     `;
   } catch (error) {
     // If a database error occurs, return a more specific error.
-    console.error("Error creating invoice:", error);
+    return {
+      message: "Database Error: Failed to Create Invoice.",
+    };
   }
 
   // Revalidate the cache for the invoices page and redirect the user.
